6c32bc419b839ef7908fac91ca294914ff022251
navneet-chugh <navneetchugh15@gmail.com>
2023-08-21 23:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
+
+const form = document.getElementById("form1") ;
+
+
 function getFormvalue() {
-    const form = document.getElementById('form1');
-    const firstName = form.fname.value;
-    const lastName = form.lname.value;
+    let fullName = form["fname"].value + " "+ form["lname"].value ;
+  alert(fullName);
 
-    if (firstName && lastName) {
-        alert(firstName + lastName);
-    } else {
-        alert("Please enter both First Name and Last Name.");
-    }
 }
-
-// Add an event listener to the form to call the function on submission
-document.getElementById('form1').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    getFormvalue();
-});
