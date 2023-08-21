901ad50e4583e53688f5a1ad290c35fd9f2aedeb
navneet-chugh <navneetchugh15@gmail.com>
2023-08-21 22:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function getFormvalue() {
-    //Write your code here
-	const form = document.getElementById('form1');
-  const firstName = form.fname.value;
-  const lastName = form.lname.value;
+    const form = document.getElementById('form1');
+    const firstName = form.fname.value;
+    const lastName = form.lname.value;
 
-  if (firstName && lastName) {
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
-  } else {
-    alert("Please enter both First Name and Last Name.");
-  }
+    if (firstName && lastName) {
+        alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    } else {
+        alert("Please enter both First Name and Last Name.");
+    }
 }
+
 // Add an event listener to the form to call the function on submission
 document.getElementById('form1').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the default form submission
-  getFormvalue();
+    event.preventDefault(); // Prevent the default form submission
+    getFormvalue();
 });
